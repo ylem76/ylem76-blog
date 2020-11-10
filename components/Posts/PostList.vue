35e540bdf5fd4f529a53f57e32e4b0854a7035f0
ylem76 <ylem76@thebreeze.co.kr>
2020-11-10 18:40:47 +0900

    20201110 first

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="post-list">
+        <PostPreview id="1" title="first-title"
+            thumbnail="https://images.pexels.com/photos/5054213/pexels-photo-5054213.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            previewText="This is my first post!" />
+        <PostPreview id="2" title="second-title"
+            thumbnail="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            previewText="This is my second post!" />
+        <PostPreview id="3" title="third-title"
+            thumbnail="https://images.pexels.com/photos/4050318/pexels-photo-4050318.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            previewText="This is my third post!" />
+    </section>
+</template>
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+    component: {
+        PostPreview
+    }
+    
+}
+</script>
+<style scoped>
+    .post-list {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        
+    }
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="admin-page">
+        <section class="new-post">
+            <button>새 글쓰기</button>
+        </section>
+        <section class="existing-posts">
+            <h1>현재 블로그 글 목록</h1>
+            <PostList />
+        </section>
+    </div>
+</template>
+<script>
+import PostList from '@/components/Posts/PostList'
+export default {
+    components : {
+        PostList
+    }
+    
+}
+</script>
+<style scoped>
+    .admin-page {
+        padding: 20px;
+    }
+
+    .new-post {
+        text-align: center;
+        border-bottom: 2px solid #ccc;
+        padding-bottom: 10px;
+    }
+
+    .existing-posts h1 {
+        text-align: center;
+    }
+</style>
