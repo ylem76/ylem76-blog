80dc571cc188d6c555f0aec37fc0bcf98531ae77
ylem76 <ylem76@thebreeze.co.kr>
2020-11-12 18:12:32 +0900

    파이어베이스 포스트 목록보기 완성

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -2,7 +2,8 @@
     <form @submit.prevent="onSave">
         <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
         <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
+        <AppControlInput v-model="editedPost.previewText">previewText</AppControlInput>
         <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
         <AppButton type="submit">Save</AppButton>
         <AppButton type="button" style="margin-left: 10px" btn-style="cancel" @click="onCancel">Cancel
@@ -34,7 +35,8 @@
                 } : {
                     author: '',
                     title: '',
-                    thumbnailLink: '',
+                    thumbnail: '',
+                    previewText: '',
                     content: ''
                 }
             }
